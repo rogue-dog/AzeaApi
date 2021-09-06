38e9ad4377b5e05c82c2982b237b11346f72f033
rogue-dog <85028783+rogue-dog@users.noreply.github.com>
2021-09-06 22:28:30 +0530

    zzse

--- UserApi/admin.py
+++ UserApi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, UserVerification
+from .models import User, UserVerification, Post
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserVerification)
+admin.site.register(Post)
